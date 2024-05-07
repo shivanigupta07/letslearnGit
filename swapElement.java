9c1de3caf4d5ad80859bbb5fd0dbd7195217f1cd
shivanigupta07 <guptashivani111111@gmail.com>
2024-05-07 10:07:02 +0530

    file saved

---
+++ swapElement.java
@@ -0,0 +1,15 @@
+public class swapElement {
+    public static void main(String args[]){
+int a=10;
+int b=20;
+//a=a^b;
+//b=a^b;
+//a=a^b;
+int temp=0;
+temp=a;
+a=b;
+b=temp;
+System.out.println("After swapping a="+ a +"and value of b="+b);
+
+        }
+    }
